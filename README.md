407064704d5940db90d9dd0774986842f5311c7e
vdndev <developpement.vandernoot@gmail.com>
2023-01-12 22:09:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+﻿## TP 1 : Jeu de Combat
+
+On veut créer les bases d'un jeu de combat d'arêne Joueur contre Joueur.
+
+Pour cela, vous réaliserez le MCD-MLD selon le cahier des charges suivants :
+
+- Chaque joueur doit pouvoir se connecter, avec un pseudo qui sera unique, un mot de passe, et son mail.
+- Chaque joueur pourra demander un ou plusieurs autres joueur en ami.
+- Chaque joueur aura une messagerie pour communiquer avec d'autres joueurs. On aura besoin de connaître le sujet du message, et le contenu du message.
+- Chaqhue joueur pourra se créer plusieurs personnages. Les personnages auront un nom et une classe de personnage (ex: Barbare, Chevalier, Guerrier).
+- Chaque classe donnera des statistique de base : Point de Vie, Attaque, Défense.
+- En plus de leur classe, les personnages pourront avoir des objets dans leur inventaire. Un objet pourra être équipé. Et une fois équipé, il fournira au personnage des bonus en terme de point de vie,
+  d'attaque et de défense.
+- Les personnages s'affrontent en duel (un contre un) dans une arêne. Il y a plusieurs arênes disponibles, et le résultat du combat est enregistré dans un journal qui répertorie la date du combat,
+  le vainqueur, le vaincue, et l'arêne du duel.
+- Enfin, chaque joueur peut acheter un ou plusieurs costumes pour un de ses personnages (chaque achat ne concerne qu'un seul des personnages du joueur).
+
+## TP 2 : Remonter le MCD à partir d'un SQL
+
+A partir du SQL ci-dessous, recréez le MCD d'origine.
+
+Vous l'enregistrerez et l'enverrez au formateur.
+
+Puis, rédigez ce qui vous semble incorrecte dans le MCD, et proposez un MCD corrigé.
---
+++ bdd.sql
@@ -0,0 +1,165 @@
+﻿CREATE TABLE article_sport(
+   Id_article_sport INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+   titre_article VARCHAR(50),
+   contenu_article VARCHAR(50),
+   image_article VARCHAR(50)
+);
+
+CREATE TABLE article_cinema(
+   Id_article_cinema INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+   titre_article VARCHAR(50),
+   contenu_article VARCHAR(50),
+   image_article VARCHAR(50)
+);
+
+CREATE TABLE article_serie(
+   Id_article_serie INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+   titre_article VARCHAR(50),
+   contenu_article VARCHAR(50),
+   image_article VARCHAR(50)
+);
+
+CREATE TABLE visiteur(
+   Id_visiteur INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+   nom_visiteur VARCHAR(50),
+   prenom_visiteur VARCHAR(50),
+   mdp_visiteur VARCHAR(50),
+   login_visiteur VARCHAR(50)
+);
+
+CREATE TABLE admin(
+   Id_admin INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+   nom_admin VARCHAR(50),
+   prenom_admin VARCHAR(50),
+   mdp_admin VARCHAR(50),
+   login_admin VARCHAR(50)
+);
+
+CREATE TABLE message(
+   Id_message INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+   objet_message VARCHAR(50),
+   contenu_message VARCHAR(50),
+   Id_visiteur INT NOT NULL,
+   FOREIGN KEY(Id_visiteur) REFERENCES visiteur(Id_visiteur)
+);
+
+CREATE TABLE commentaire(
+   Id_commentaire INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+   texte_commentaire VARCHAR(50)
+);
+
+CREATE TABLE commentaire_visiteur(
+   Id_commentaire_visiteur INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+   texte_commentaire VARCHAR(50)
+);
+
+CREATE TABLE mot_clef_article(
+   Id_mot_clef INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+   sport VARCHAR(50),
+   cinema VARCHAR(50),
+   serie VARCHAR(50)
+);
+
+CREATE TABLE premium(
+   Id_premium INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+   article_sport_premium VARCHAR(50),
+   article_cinema_premium VARCHAR(50),
+   article_serie_premium VARCHAR(50)
+);
+
+CREATE TABLE produit(
+   Id_produit INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+   nom_produit VARCHAR(50),
+   prix_produit VARCHAR(50)
+);
+
+CREATE TABLE ticket(
+   Id_ticket INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+   quantite VARCHAR(50)
+);
+
+CREATE TABLE auteur(
+   Id_auteur INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+   nom_auteur VARCHAR(50),
+   prenom_auteur VARCHAR(50),
+   mdp_auteur VARCHAR(50),
+   login_utilisateur VARCHAR(50),
+   Id_article_serie INT NOT NULL UNIQUE,
+   Id_article_cinema INT NOT NULL UNIQUE,
+   Id_article_sport INT NOT NULL UNIQUE,
+   FOREIGN KEY(Id_article_serie) REFERENCES article_serie(Id_article_serie),
+   FOREIGN KEY(Id_article_cinema) REFERENCES article_cinema(Id_article_cinema),
+   FOREIGN KEY(Id_article_sport) REFERENCES article_sport(Id_article_sport)
+);
+
+CREATE TABLE vendeur(
+   Id_vendeur INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+   nom_vendeur VARCHAR(50),
+   prenom_vendeur VARCHAR(50),
+   Id_produit INT UNIQUE,
+   FOREIGN KEY(Id_produit) REFERENCES produit(Id_produit)
+);
+
+CREATE TABLE voir(
+   Id_article_sport INT,
+   Id_visiteur INT,
+   PRIMARY KEY(Id_article_sport, Id_visiteur),
+   FOREIGN KEY(Id_article_sport) REFERENCES article_sport(Id_article_sport),
+   FOREIGN KEY(Id_visiteur) REFERENCES visiteur(Id_visiteur)
+);
+
+CREATE TABLE lire(
+   Id_article_cinema INT,
+   Id_visiteur INT,
+   PRIMARY KEY(Id_article_cinema, Id_visiteur),
+   FOREIGN KEY(Id_article_cinema) REFERENCES article_cinema(Id_article_cinema),
+   FOREIGN KEY(Id_visiteur) REFERENCES visiteur(Id_visiteur)
+);
+
+CREATE TABLE consulter(
+   Id_article_serie INT,
+   Id_visiteur INT,
+   PRIMARY KEY(Id_article_serie, Id_visiteur),
+   FOREIGN KEY(Id_article_serie) REFERENCES article_serie(Id_article_serie),
+   FOREIGN KEY(Id_visiteur) REFERENCES visiteur(Id_visiteur)
+);
+
+CREATE TABLE gerer(
+   Id_auteur INT,
+   Id_admin INT,
+   PRIMARY KEY(Id_auteur, Id_admin),
+   FOREIGN KEY(Id_auteur) REFERENCES auteur(Id_auteur),
+   FOREIGN KEY(Id_admin) REFERENCES admin(Id_admin)
+);
+
+CREATE TABLE administrer(
+   Id_visiteur INT,
+   Id_admin INT,
+   PRIMARY KEY(Id_visiteur, Id_admin),
+   FOREIGN KEY(Id_visiteur) REFERENCES visiteur(Id_visiteur),
+   FOREIGN KEY(Id_admin) REFERENCES admin(Id_admin)
+);
+
+CREATE TABLE commenter(
+   Id_auteur INT,
+   Id_commentaire INT,
+   PRIMARY KEY(Id_auteur, Id_commentaire),
+   FOREIGN KEY(Id_auteur) REFERENCES auteur(Id_auteur),
+   FOREIGN KEY(Id_commentaire) REFERENCES commentaire(Id_commentaire)
+);
+
+CREATE TABLE poster(
+   Id_visiteur INT,
+   Id_commentaire_visiteur INT,
+   PRIMARY KEY(Id_visiteur, Id_commentaire_visiteur),
+   FOREIGN KEY(Id_visiteur) REFERENCES visiteur(Id_visiteur),
+   FOREIGN KEY(Id_commentaire_visiteur) REFERENCES commentaire_visiteur(Id_commentaire_visiteur)
+);
+
+CREATE TABLE vendre(
+   Id_vendeur INT,
+   Id_ticket INT,
+   PRIMARY KEY(Id_vendeur, Id_ticket),
+   FOREIGN KEY(Id_vendeur) REFERENCES vendeur(Id_vendeur),
+   FOREIGN KEY(Id_ticket) REFERENCES ticket(Id_ticket)
+);
